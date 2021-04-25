a2785df55482b857adc7cdca579831695d945827
realBigpac <52135684+realBigpac@users.noreply.github.com>
2021-04-25 00:23:40 -0500

    added most of the webpage

---
+++ incjs/project.js
@@ -0,0 +1,47 @@
+let projects = [
+  {
+    "proj_name": "mdnCanvasTutorial",
+    "proj_desc": "A canvas I made with my name and a few shape elements with an image placed sporadically on the canvas.",
+    "language": "HTML",
+    "repo": "https://github.com/realBigpac/IT202-Spring2021-activities/blob/master/mdnCanvasTutorial.html",
+    "func_page": "https://realbigpac.github.io/IT202-Spring2021-activities/mdnCanvasTutorial",
+    "img": "incjs/projimgs/mdnCanvasTutorial.png",
+    "display": true
+  },
+  {
+    "proj_name": "locationAwareMap",
+    "proj_desc": "A map with the ability to track your location, location is not saved to a server or viewable by others.",
+    "language": "HTML",
+    "repo": "https://github.com/realBigpac/IT202-Spring2021-activities/blob/master/locationAwareMap.html",
+    "func_page": "https://realbigpac.github.io/IT202-Spring2021-activities/locationAwareMap",
+    "img": "incjs/projimgs/locationAwareMap.png",
+    "display": true
+  },
+  {
+    "proj_name": "mcw_basics",
+    "proj_desc": "A few basic elemts of MCW used to show a snackbar of whatever you type into the text box.",
+    "language": "HTML",
+    "repo": "https://github.com/realBigpac/IT202-Spring2021-activities/blob/master/mcw_basics.html",
+    "func_page": "https://realbigpac.github.io/IT202-Spring2021-activities/mcw_basics",
+    "img": "incjs/projimgs/mcw_basics.png",
+    "display": true
+  },
+  {
+    "proj_name": "chart_basics",
+    "proj_desc": "A pair of charts showing the types of schools in chicago and the number of doses of some vaccine",
+    "language": "HTML",
+    "repo": "https://github.com/realBigpac/IT202-Spring2021-activities/blob/master/chart_basics.html",
+    "func_page": "https://realbigpac.github.io/IT202-Spring2021-activities/chart_basics",
+    "img": "incjs/projimgs/chart_basics.png",
+    "display": true
+  },
+  {
+    "proj_name": "bootstrap_rwd_basics",
+    "proj_desc": "A very basic page set up to display columns and both flexible and fixed width bands using RWD.",
+    "language": "HTML",
+    "repo": "https://github.com/realBigpac/IT202-Spring2021-activities/blob/master/bootstrap_rwd_basics.html",
+    "func_page": "https://realbigpac.github.io/IT202-Spring2021-activities/bootstrap_rwd_basics",
+    "img": "incjs/projimgs/bootstrap_rwd_basics.png",
+    "display": true
+  }
+]
